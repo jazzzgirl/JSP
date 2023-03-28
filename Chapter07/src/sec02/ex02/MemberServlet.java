f5aab79a52c84362a5cb63b71079b12d5c1105df
KIMJY <122241455+jazzzgirl@users.noreply.github.com>
2023-03-28 13:13:20 +0900

    Add files via upload

--- Chapter07/src/sec02/ex02/MemberServlet.java
+++ Chapter07/src/sec02/ex02/MemberServlet.java
@@ -18,17 +18,48 @@ import javax.servlet.http.HttpServletResponse;
 public class MemberServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		doHandle(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doHandle(request, response);
+	}
+
+	protected void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 추가
+		request.setCharacterEncoding("utf-8");   
+		
 		response.setContentType("text/html;charset=utf-8");
-		PrintWriter out=response.getWriter();	
-		MemberDAO dao=new MemberDAO();
-		List<MemberVO> list=dao.listMembers();
-	
-		out.print("<html><body>");
-		out.print("<table  border=1><tr align='center' bgcolor='lightgreen'>");
-		out.print("<td>아이디</td><td>비밀번호</td><td>이름</td><td>이메일</td><td>가입일</td></tr>");
-     
+	    PrintWriter out = response.getWriter();	
+	    MemberDAO dao = new MemberDAO();
+	    
+	    // 추가
+	    String command=request.getParameter("command");
+	    if(command!= null && command.equals("addMember")){
+			 String _id = request.getParameter("id");
+			 String _pwd = request.getParameter("pwd");
+			 String _name = request.getParameter("name");
+			 String _email = request.getParameter("email");
+			 
+			 MemberVO vo=new MemberVO();
+			 vo.setId(_id);
+			 vo.setPwd(_pwd);
+			 vo.setName(_name);
+			 vo.setEmail(_email);
+		     dao.addMember(vo);
+	    }
+	    
+	    List<MemberVO> list = dao.listMembers();   
+		
+	    out.print("<html><body>");
+	    out.print("<table  border=1><tr align='center' bgcolor='lightgreen'>");
+	    out.print("<td>아이디</td><td>비밀번호</td><td>이름</td><td>이메일</td><td>가입일</td></tr>");
+	     
 		for (int i=0; i<list.size();i++){
 			MemberVO memberVO=(MemberVO) list.get(i);
 			String id=memberVO.getId();
@@ -37,12 +68,14 @@ public class MemberServlet extends HttpServlet {
 			String email=memberVO.getEmail();
 			Date joinDate = memberVO.getJoinDate();
 			out.print("<tr><td>"+id+"</td><td>"+
-				                pwd+"</td><td>"+
-				                name+"</td><td>"+
-				                email+"</td><td>"+
-				                joinDate+"</td></tr>");		
-      }
-      out.print("</table></body></html>");
+		                pwd+"</td><td>"+
+		                name+"</td><td>"+
+		                email+"</td><td>"+
+		                joinDate+"</td></tr>");		
+		}
+		out.print("</table></body></html>");
+		
+		// 추가
+		out.print("<a href='/Chapter07/memberForm.html'>새 회원 등록하기</a>");  
 	}
-
 }
