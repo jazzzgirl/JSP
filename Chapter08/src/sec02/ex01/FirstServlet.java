cfe01f8569d9d73f1852f774f204cd783404669c
KIMJY <122241455+jazzzgirl@users.noreply.github.com>
2023-03-29 23:36:17 +0900

    Add files via upload

---
+++ Chapter08/src/sec02/ex01/FirstServlet.java
@@ -0,0 +1,24 @@
+package sec02.ex01;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/first7")
+public class FirstServlet extends HttpServlet{
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+				
+		response.setContentType("text/html;charset=utf-8");
+		      
+		request.setAttribute("address", "부산시 사상구 괘법동");
+		RequestDispatcher dispatch = request.getRequestDispatcher("second7");
+		dispatch.forward(request, response);
+   }
+}
+
---
+++ Chapter08/src/sec02/ex02/MemberServlet.java
@@ -0,0 +1,38 @@
+package sec02.ex02;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Member8")  // 수정
+public class MemberServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+	
+	protected void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");   
+		
+		response.setContentType("text/html;charset=utf-8");
+	    PrintWriter out = response.getWriter();	
+	    MemberDAO dao = new MemberDAO();
+	    
+	    // 추가
+	    List membersList = dao.listMembers();   
+		
+	    request.setAttribute("membersList", membersList);
+		RequestDispatcher dispatch = request.getRequestDispatcher("viewMembers");
+		dispatch.forward(request, response);
+	}
+}
