6107ebb2579f4e3cbaa7302b128861de1438bb2e
KIMJY <122241455+jazzzgirl@users.noreply.github.com>
2023-04-06 09:31:23 +0900

    Add files via upload

---
+++ Chapter08/src/sec03/ex02/ContextParamServlet.java
@@ -0,0 +1,35 @@
+package sec03.ex02;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/initMenu")
+public class ContextParamServlet extends HttpServlet {	
+	protected  void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException,  IOException {
+      request.setCharacterEncoding("utf-8");
+      response.setContentType("text/html;charset=utf-8");
+      PrintWriter out = response.getWriter();
+      
+      // ServletContext 객체 가져 옴.
+      ServletContext context = getServletContext();
+
+      // web.xml의 <param-name>태그의 이름으로 <param-value> 태그의 값인 메뉴 이름들을 받아 옴.
+      String menu_member = context.getInitParameter("menu_member");
+      String menu_order = context.getInitParameter("menu_order");
+      String menu_goods =context.getInitParameter("menu_goods");
+
+      out.print("<html><body>");
+      out.print("<table border=1 cellspacing=0><tr>메뉴 이름</tr>");
+      out.print("<tr><td>" + menu_member + "</td></tr>");
+      out.print("<tr><td>" + menu_order + "</td></tr>");
+      out.print("<tr><td>" + menu_goods + "</td></tr>");
+      out.print("</table></body></html>");	
+   }
+}
