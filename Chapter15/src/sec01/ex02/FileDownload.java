6130fcc5f8dd37c28deda4e3a0c613b5c81f43f4
KIMJY <122241455+jazzzgirl@users.noreply.github.com>
2023-05-25 13:55:52 +0900

    Update FileDownload.java

--- Chapter15/src/sec01/ex02/FileDownload.java
+++ Chapter15/src/sec01/ex02/FileDownload.java
@@ -28,18 +28,13 @@ public class FileDownload extends HttpServlet {
 		String file_updn = "C:\\file_updn";
 		
 		// 매개변수로 전송된 이름을 읽어옴.
-		String fileName = (String) request.getParameter("fileName");
-		System.out.println("fileName=" + fileName);
+		
 		
 		// response에서 OutputStream 객체 가져옴.
-		OutputStream out = response.getOutputStream();
-		String downFile = file_updn + "\\" + fileName;
-		File f = new File(downFile);
+		
 		
 		// 파일 다운로드 
-		response.setHeader("Cache-Control", "no-cache");
-		response.addHeader("Content-disposition", "attachment; fileName=" + fileName);
-		FileInputStream in = new FileInputStream(f);
+		
 		
 		// 버퍼 기능을 이용해 파일에서 버퍼로 데이터를 읽어와 한번에 출력.
 		byte[] buffer = new byte[1024 * 8];
